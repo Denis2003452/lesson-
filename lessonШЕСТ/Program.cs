fcba126fe0cff9606cd95506bb7173c337f37051
Denis Ishenko <ddva.dpa@mail.ru>
2022-12-03 18:49:52 +0300

    Добавьте файлы проекта.

---
+++ lessonШЕСТ/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace lessonШЕСТ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+   public class Person
+    {
+        public string Name { get; }     
+                      
+       
+        public int Age { get; }
+
+        public int Salary;
+
+        public Person (string name, int age, int salary )
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        
+    }
+    public class Scientist : Person
+    {
+       
+        public Scientist(string name, int age, int salary, string academicdegree) : base(name, age, salary)
+        {
+            AcademicDegree = academicdegree;
+        }
+
+        public string AcademicDegree { get; }
+        
+            
+
+        public void WriteCharacteristics(Scientist person)
+        {
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Salary);
+            Console.WriteLine(person.AcademicDegree);
+        }
+        
+    }
+}
